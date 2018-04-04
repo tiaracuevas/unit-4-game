5568c2c2240994c066ba885f71833b5c9e915285
Tiara Cuevas <tiara.handy@gmail.com>
2018-04-04 15:56:45 -0400

    pushing game

---
+++ assets/javascript/game.js
@@ -0,0 +1,116 @@
+$(document).ready(function() {
+//generate random number//
+var randomNumber= Math.floor((Math.random() * 120) + 19);
+console.log(randomNumber);
+$('.random-number').text("Target number: " + randomNumber)
+
+
+//generate crystal numbers
+var crystalOne= Math.floor((Math.random() * 12) + 1);
+    console.log(crystalOne)
+
+var crystalTwo= Math.floor((Math.random() * 12) + 1);
+    console.log(crystalTwo)
+
+var crystalThree= Math.floor((Math.random() * 12) + 1);
+    console.log(crystalThree)
+
+var crystalFour= Math.floor((Math.random() * 12) + 1);
+    console.log(crystalFour)
+
+//counters//
+var winCounter= 0;
+var lossCounter= 0;
+var totalScore= 0;
+
+
+//reset game//
+function reset (){
+    randomNumber= Math.floor((Math.random() * 120) + 19);
+    //display random number//
+    $('.random-number').text("Target number:" + randomNumber);
+    //do not display crystal values//
+    crystalOne= Math.floor((Math.random() * 12) + 1);
+    crystalTwo= Math.floor((Math.random() * 12) + 1);
+    crystalThree= Math.floor((Math.random() * 12) + 1);
+    crystalFour= Math.floor((Math.random() * 12) + 1);
+    //reset total score & display//
+    totalScore= 0;
+    $('#totalScore').text("Your total score is: " + totalScore);
+}
+
+//display win/loss/total counters
+$('#wins').text("Wins: " + winCounter);
+$('#losses').text("Losses: " + lossCounter);
+$('#totalScore').text("Your total score is: " + totalScore);
+
+//win and loss functions//
+function win (){
+    alert("Winner!")
+    winCounter++;
+    $('#wins').text("Wins: " + winCounter);
+    reset ();
+}
+
+function loss (){
+    alert("You Lose!")
+    lossCounter++;
+    $('#losses').text("Losses: " + lossCounter);
+    reset ();
+}
+
+//click functions for crystal buttons//
+//crystal button 1//
+$('#crystal-one').on ('click', function (){
+    totalScore= crystalOne + totalScore;
+    $('#totalScore').text("Your total score is: " + totalScore);
+
+    if (totalScore == randomNumber) {
+        win();
+    }
+    else if (totalScore > randomNumber) {
+        loss ();
+    }
+});
+
+//crystal button 2//
+$('#crystal-two').on ('click', function (){
+    totalScore= crystalTwo + totalScore;
+    $('#totalScore').text("Your total score is: " + totalScore);
+
+    if (totalScore == randomNumber) {
+        win();
+    }
+    else if (totalScore > randomNumber) {
+        loss ();
+    }
+});
+
+//crystal button 3//
+$('#crystal-three').on ('click', function (){
+    totalScore= crystalThree + totalScore;
+    $('#totalScore').text("Your total score is: " + totalScore);
+
+    if (totalScore == randomNumber) {
+        win();
+    }
+    else if (totalScore > randomNumber) {
+        loss ();
+    }
+});
+
+//crystal button 4//
+$('#crystal-four').on ('click', function (){
+    totalScore= crystalFour + totalScore;
+    $('#totalScore').text("Your total score is: " + totalScore);
+
+    if (totalScore == randomNumber) {
+        win();
+    }
+    else if (totalScore > randomNumber) {
+        loss ();
+    }
+});
+
+
+});
